ad841c97a203e8ccbeb5a13822c01e84eb4b423c
DigitalJsn <92397083+DigitalJsn@users.noreply.github.com>
2025-12-30 21:31:24 -0500

    saved all files

--- dist/script.js
+++ dist/script.js
@@ -2,6 +2,7 @@ const shareButton = document.getElementById('share-button');
 const shareModal = document.getElementById('open-modal');
 const closeModal = document.getElementById('close-modal');
 const modalContainer = document.querySelector('.modal-container');
+
 shareButton.addEventListener('click', openModal);
 closeModal.addEventListener('click', close);
 modalContainer.addEventListener('click', close);
@@ -10,10 +11,8 @@ modalContainer.addEventListener('click', close);
 function openModal() {
     shareModal.style.display = 'block';
     shareButton.style.zIndex = '0';
-    // alert('clicked');
 }
 function close() {
     shareButton.style.zIndex = '1';
     shareModal.style.display = 'none';
-    // window.addEventListener('click', close);
 }
